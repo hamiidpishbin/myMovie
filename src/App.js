5aba7368021f2a1d803ed26c3db57a9bdb179aaf
Hamid Pishbin <hamidpishbin89@gmail.com>
2023-10-30 21:36:04 +0330

    Return Simple List of Movies Titles

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React, { useState, useEffect } from "react";
 import { getAllMovies } from "./components/Transportlayer";
 
 function App() {
-  
-  return <div>Hiii</div>
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getAllMovies()
+      .then((data) => {
+        setMovies(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
